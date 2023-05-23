c94cc418e3d979196095c1fd4b83e059be65423f
Anatolii Yakushko <ayakushko@playtika.com>
2023-05-23 15:53:41 +0300

    [-] small fixes

--- src/adc/adc.cpp
+++ src/adc/adc.cpp
@@ -4,7 +4,7 @@
 int getAdcValue() {
     uint16_t result = 0;
     for (int i = 0; i < TEMP_NUM_READS; i++){
-       result += analogRead((analog_pin_t) THERMORESISTOR_PIN) * 10;
+       result += analogRead(THERMORESISTOR_PIN) * 10;
        tiny_delay(10);
     }
     return (result / TEMP_NUM_READS) / 10;
