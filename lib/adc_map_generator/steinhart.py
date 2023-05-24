17119d5ad011bd28080346667aed7d504873ffbb
Anatolii Yakushko <ayakushko@playtika.com>
2023-05-24 13:25:21 +0300

    [-] removed redundand code

--- lib/adc_map_generator/steinhart.py
+++ lib/adc_map_generator/steinhart.py
@@ -8,14 +8,10 @@ class SteinHart:
         self.conf = conf
 
     def resistance_to_temp(self, resistance):
-        steinhart = float(resistance) / self.conf.THERMISTORNOMINAL
+        steinhart = resistance / self.conf.THERMISTORNOMINAL
         steinhart = math.log(steinhart)
-        steinhart /= float(self.conf.BCOEFFICIENT)
-        steinhart += 1.0 / (self.conf.TEMPERATURENOMINAL + 273.15)
-        steinhart = 1.0 / steinhart
+        steinhart /= self.conf.BCOEFFICIENT
+        steinhart += 1 / (self.conf.TEMPERATURENOMINAL + 273.15)
+        steinhart = 1 / steinhart
         steinhart -= 273.15
         return steinhart
-
-    def _calc_r(self, adc_value):
-        reading = float(float(self.conf.ADC_RANGE - 1) / adc_value) - 1
-        return float(self.conf.SERIESRESISTOR) / reading
